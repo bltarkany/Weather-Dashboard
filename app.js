e72163c4f224b857a9dc49a645b168a4b34df409
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-07-13 10:09:02 -0700

    added css page. finished layout of index.html. added notes and start of js

---
+++ app.js
@@ -0,0 +1,15 @@
+// global variable
+let city = 
+
+// global functions
+
+// function to call for todays weather
+
+// function to call todays UV index
+
+// function to call for 5 day forecast
+
+
+$(document).ready(function() {
+
+});
