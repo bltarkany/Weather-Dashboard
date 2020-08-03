07f62861541de747e929e9788443a99a1c822234
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-08-03 10:51:04 -0700

    created newcity fucntion with buton creation and added to frontend. added styling to button in css.

--- app.js
+++ app.js
@@ -8,12 +8,27 @@ const apiKey = "91a8aba9a4e777059b0459d4f649ec84";
 
 // function to create history buttons
 function newCity(city) {
+    // create button with the value of the city and styling
+    let cityBtn = $('<button>')
+        .val(city)
+        .addClass('waves-effect waves-teal btn-flat city-btn')
+        .text(city);
+    // add onClick to each button 
+    cityBtn.onClick = handleOnClick;
+    // append to frontend card
+    $('#history').append(cityBtn);
+    // add the local storage
 
 }
 
 // load history from local storage to populate buttons
 function history() {
-    
+
+}
+
+// handle history button onclick
+function handleOnClick() {
+
 }
 
 // function to call for todays weather
@@ -40,7 +55,7 @@ $(document).ready(function () {
         let city = $('#city').val().trim();
         console.log(city);
         // run the functions with the city
-
+        newCity(city);
         // empty the value in the input section
         $('#city').val('');
     });
