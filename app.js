1e83503b7dda57462a06b597f7665c50c9659e8d
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-08-03 10:16:33 -0700

    added card holders to index.html. added font color to css. created function names and pulled city value in app.js

--- app.js
+++ app.js
@@ -1,27 +1,47 @@
 // global variable
+const apiKey = "91a8aba9a4e777059b0459d4f649ec84";
 
-// grab the value from the input
-let city = $('#search').val();
+
+
 
 // global functions
 
 // function to create history buttons
+function newCity(city) {
+
+}
+
+// load history from local storage to populate buttons
+function history() {
+    
+}
 
 // function to call for todays weather
+function weather(city) {
+
+}
 
 // function to call todays UV index
+function uv(city) {
 
+}
 // function to call for 5 day forecast
+function forecast(city) {
+
+}
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // on click event for search city
-    $('#searchBtn').on('click', function() {
+    $('#search-btn').on('click', function () {
+        console.log('search clicked');
+        // grab the value from the input
+        let city = $('#city').val().trim();
         console.log(city);
         // run the functions with the city
 
         // empty the value in the input section
-
+        $('#city').val('');
     });
 });
