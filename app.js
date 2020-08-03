de16720044709fb5cd01514790990443beb1c11c
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-08-02 20:35:15 -0700

    added backgroud image, changed front end search appearance, changed from bootstrap to materialize.

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // global variable
-let city = 
+
+// grab the value from the input
+let city = $('#search').val();
 
 // global functions
+
+// function to create history buttons
 
 // function to call for todays weather
 
@@ -12,4 +16,12 @@ let city =
 
 $(document).ready(function() {
 
+    // on click event for search city
+    $('#searchBtn').on('click', function() {
+        console.log(city);
+        // run the functions with the city
+
+        // empty the value in the input section
+
+    });
 });
